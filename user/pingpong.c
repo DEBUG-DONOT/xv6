5f2d18b2d6f0cc023fdeb0c2c46dbd1aca642977
DEBUG-DONOT <877845066@qq.com>
2024-07-27 21:31:44 +0800

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,62 +1,31 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-char* itoa(int value, char* str) {
-    char* pos = str;
-    if (value < 0) {
-        *pos++ = '-';
-        value = -value;
-    }
-    int digit = value % 10;
-    if ((value / 10) > 0) {
-        pos = itoa(value / 10, pos);
-    }
-    *pos++ = digit + '0';
-    *pos = '\0';
-    return str;
-}
+
 int main()
 {
-  int ping[2];
-  int pong[2];
-  int op[2];
-  close(0);
-  close(1);
-  pipe(op);
-  pipe(ping);
-  pipe(pong);
-  int pid=fork();
-  if(pid==0)//子进程
-  {
-    //close(1);
-  
-    int childPid=getpid();
-    char cc[512];
-    itoa(childPid,cc);
-    write(pong[1],"pong",4);
-    close(pong[0]);
-    close(pong[1]);
-    char buf[4];
-    read(ping[0],buf,4);
-    write(op[1],cc,strlen(cc));
-    write(op[1],": received ping\n",17);
-    exit(0);
-  }
-  else//父进程
-  {
-    //wait(&pid);加wait会死锁
+    int p[2];
+    pipe(p);
+    int pid=fork();
+    if(pid==0)
+    {
+        
+        char buf[4];
+        read(p[1],buf,4);
+        printf("%d: received ping\n",getpid());
+        write(p[0],"pong",4);
+        exit(0);
+    }
+    else
+    {
+        write(p[1],"ping",4);
+        close(p[1]);
+        wait(&pid);
+        char buf[4];
+        read(p[0],buf,4);
+        close(p[0]);
+        printf("%d: received pong\n",getpid());
+        exit(0);
+    }
 
-    int parentPid=getpid();
-    char cp[512];
-    char buf[4];
-    itoa(parentPid,cp);
-    read(pong[0],buf,4);
-    write(op[1],cp,strlen(cp));
-    write(op[1],": received pong\n",17);
-
-    write(ping[1],"ping",4);
-    close(ping[0]);
-    close(ping[1]);
-    exit(0);
-  }
 }
